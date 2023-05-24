24cd73b7587de2f16a0392d0e0214a79be34dbdc
Tomislav Busic <tomislav.busic89@gmail.com>
2023-05-24 21:32:02 +0200

    userData

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 var ip = "86.33.86.100";
 var access_key = "6cff20186044ce2be984438909bddd97";
 let userData = [];
+let title = document.querySelector("span");
 
 const fetchData = async () => {
   await fetch(`http://api.ipstack.com/${ip}?access_key=${access_key}`)
@@ -8,11 +9,13 @@ const fetchData = async () => {
     .then((data) => {
       userData = [data];
       console.log(userData);
-      localStorage.setItem(
-        "country",
-        JSON.stringify(userData[0]?.country_name)
-      );
+      localStorage.setItem("country", userData[0]?.country_name);
+      showCountry();
     });
+};
+
+const showCountry = () => {
+  title.innerText = localStorage.getItem("country");
 };
 
 fetchData();
